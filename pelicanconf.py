d9b44bd06555cb7a59ab8028af12c3fdc1e3a921
Matt Gibson <contact.matt.gibson@gmail.com>
2024-01-15 21:25:13 +1100

    (refactor) more cleanup

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,17 +2,19 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'Matt Gibson'
-SITENAME = u'Matt Gibson'
-SITEURL = 'http://www.matthew-gibson.com'
 
-PATH = 'content'
+# #### Basic settings ####
+AUTHOR = "Matt Gibson"
+SITENAME = "Matt Gibson"
+SITEURL = "http://www.matthew-gibson.com"
 
-TIMEZONE = 'Australia/Sydney'
+PATH = "content"
 
-DEFAULT_LANG = u'en'
+TIMEZONE = "Australia/Sydney"
 
-DEFAULT_DATE_FORMAT = '%Y %B %d'
+DEFAULT_LANG = "en"
+
+DEFAULT_DATE_FORMAT = "%Y %B %d"
 
 
 # these features
@@ -29,11 +31,10 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 
-
-STATIC_PATHS = ['images', 'extra/robots.txt', 'extra/favicon.ico']
+STATIC_PATHS = ["images", "extra/robots.txt", "extra/favicon.ico"]
 EXTRA_PATH_METADATA = {
-    'extra/robots.txt': {'path': 'robots.txt'},
-    'extra/favicon.ico': {'path': 'favicon.ico'}
+    "extra/robots.txt": {"path": "robots.txt"},
+    "extra/favicon.ico": {"path": "favicon.ico"},
 }
 
 
@@ -44,17 +45,20 @@ EXTRA_PATH_METADATA = {
 #         )
 
 # Social widget
-SOCIAL = (('linkedin', 'https://au.linkedin.com/in/mgibson1'),
-          ('github', 'https://github.com/'),)
+# SOCIAL = (
+#     ("linkedin", "https://au.linkedin.com/in/mgibson1"),
+#     ("github", "https://github.com/"),
+# )
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
 
-# Use theme
 
-THEME = '/Users/matt/projects/mattsite_2.0/themes/pelican-svbhack'
-USER_LOGO_URL = SITEURL + '/images/ok_photo.JPG'
-GOOGLE_ANALYTICS = 'UA-36248181-1'
-TAGLINE = 'Code monkey who enjoys accumulating technical knowledge.'
+# #### Theme settings ####
+THEME = "notmyidea"
+# THEME = "themes/my-basic"
+# USER_LOGO_URL = SITEURL + "/images/ok_photo.JPG"
+# GOOGLE_ANALYTICS = "UA-36248181-1"
+# TAGLINE = "Code monkey who enjoys accumulating technical knowledge."
