21c52fa0f7ddc64c2c1ed5dc59b1bd033dada330
Matt Gibson <contact.matt.gibson@gmail.com>
2024-01-16 13:59:48 +1100

    (docs) update documentation

--- README.md
+++ README.md
@@ -2,31 +2,46 @@
 
 This is contains the code I was using make my personal website hosted on github pages. It contains a lot cruft.
 
-## Build website
+## Setup repo
 
-Compile the website from source with
+Current bizarre workflow is
 
 ```
-pelican content
+git clone git@github.com:urbanophile/urbanophile.github.io.git
+mv urbanophile.github.io output
+```
+
+## Build dev site
+
+Build using the dev settings with
+
+```
+pelican content -s pelicanconf.py
+```
+
+Serve the site locally with:
+
+```
+pelican --autoreload --listen
+```
+
+and navigate to <http://localhost:8000/>
+
+## Build prod site
+
+Compile production site with
+
+```
+pelican content -s publishconf.py
 ```
 
 then deploy new changes with
 
 ```
-cd output 
-git add . 
-git commit -m "a change to my website" 
+cd output
+git add .
+git commit -m "a change to my website"
 git push origin master
 ```
 
 output is a separate repo for website which is a git submodule. Also the theme is separate submodule, so don't forget to clone that.
-
-## Preview website
-
-Use the following command to preview the website locally:
-
-```
-pelican --listen
-```
-
-and navigate to <http://localhost:8000/>
--- pelicanconf.py
+++ pelicanconf.py
@@ -2,11 +2,15 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+####### IMPORTANT ######
+# this is only for local development
+
 
 # #### Basic settings ####
 AUTHOR = "Matt Gibson"
 SITENAME = "Matt Gibson"
 SITEURL = "http://www.matthew-gibson.com"
+RELATIVE_URLS = True
 
 PATH = "content"
 
@@ -18,10 +22,10 @@ DEFAULT_DATE_FORMAT = "%Y %B %d"
 
 
 # these features
-AUTHOR_SAVE_AS = False
-AUTHORS_SAVE_AS = False
-CATEGORY_SAVE_AS = False
-CATEGORIES_SAVE_AS = False
+# AUTHOR_SAVE_AS = False
+# AUTHORS_SAVE_AS = False
+# CATEGORY_SAVE_AS = False
+# CATEGORIES_SAVE_AS = False
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -30,12 +34,17 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-
+# #### URL setttings ####
 STATIC_PATHS = ["images", "extra/robots.txt", "extra/favicon.ico"]
 EXTRA_PATH_METADATA = {
     "extra/robots.txt": {"path": "robots.txt"},
     "extra/favicon.ico": {"path": "favicon.ico"},
 }
+PATH_METADATA = "(?P<path_no_ext>.*)\..*"
+ARTICLES_PATH = [
+    "articles",
+]
+ARTICLE_URL = ARTICLE_SAVE_AS = PAGE_URL = PAGE_SAVE_AS = "{path_no_ext}.html"
 
 
 # Blogroll
